d8075934c151bbe1dafe44a56cfc754a0b111d49
Tushar Vyas <tusharvyas895@gmail.com>
2023-12-27 12:50:53 +0530

    Add files via upload

---
+++ App.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request
+from pymysql import connections
+import os
+import boto3
+from config import *
+
+app = Flask(__name__)
+
+db_conn = connections.Connection(
+    host=customhost,
+    port=3306,
+    user=customuser,
+    password=custompass,
+    db=customdb
+
+)
+output = {}
+table = 'student'
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    return render_template('AddStu.html')
+
+
+@app.route("/addstu", methods=['POST'])
+def AddStu():
+    emp_id = request.form['emp_id']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    dept = request.form['dept']
+    desg = request.form['desg']
+
+    insert_sql = "INSERT INTO student VALUES (%s, %s, %s, %s, %s)"
+    cursor = db_conn.cursor()
+
+    try:
+
+        cursor.execute(insert_sql, (emp_id, first_name, last_name, dept, desg))
+        db_conn.commit()
+        emp_name = "" + first_name + " " + last_name
+
+    finally:
+        cursor.close()
+
+    print("all modification done...")
+    return render_template('AddStuOutput.html', name=emp_name)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "localhost"
+customuser = "root"
+custompass = "password"
+customdb = "employee"
+custombucket = "202011075-bucket"
+customregion = "us-east-2"
